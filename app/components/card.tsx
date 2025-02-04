b73ba9fd405f8df151fa3e826a4536157e62bbf2
qwibbler <hmemaz@gmail.com>
2025-02-04 19:39:33 +0500

    Main Page

---
+++ app/components/card.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+interface CardProps {
+  header: string;
+  text?: string;
+  img?: string
+  button?: string; // Optional button text
+}
+
+export default function Card({ header, text, img, button }: CardProps) {
+  return (
+    <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+      <div className="p-4">
+        <h5 className="mb-2 text-slate-800 text-xl font-semibold">
+          {header}
+        </h5>
+
+        {text && (
+          <p className="text-slate-600 leading-normal font-light">
+            {text}
+          </p>
+        )}
+
+        {img && (
+          <Image src={img} />
+        )}
+
+        {button && (
+          <button
+            className="rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+          >
+            {button}
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,23 @@
 import Image from "next/image";
+import Logo from "@/app/components/logo";
+import Link from "next/link";
+import Card from "./components/card";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <Logo />
+        <Card header="New List"></Card>
+        <Card header="All Lists"></Card>
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
+            <Link href="/lists/new">New List</Link>
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>
+            <Link href="/lists">All Lists</Link>
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
